b6d1a99165a3f7b990e6e7c5bd0e428e293d54fc
Vibhu Rathore <vibhurathore34@gmail.com>
2025-08-24 16:38:40 +0530

    removed hardcoded URL

--- backendUrvann/src/index.js
+++ backendUrvann/src/index.js
@@ -8,10 +8,10 @@ import plantRoutes from "./routes/plantRoutes.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FrontendURL, credentials: true }));
 
 
 app.use("/", loginSignRoutes);
